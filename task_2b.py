33742e17edd48ca99fb78e2d61ea52605b458e6f
Ritesh Prakashrao Phulari <riteshprakashraop@gmail.com>
2024-05-02 18:07:07 +0530

    Image classifier model

--- task_2b.py
+++ task_2b.py
@@ -1,1 +1,132 @@
+from sys import platform
+import numpy as np
+import subprocess
+import shutil
+import ast
+import sys
+import os
+
+
+detected_list = []
+numbering_list = []
+img_name_list = []
+
+# Declaring Variables
+'''
+You can delare the necessary variables here
+'''
+
+# EVENT NAMES
+
+combat = "combat"
+rehab = "humanitarianaid"
+military_vehicles = "militaryvehicles"
+fire = "fire"
+destroyed_building = "destroyedbuilding"
+###################################################################################################
+###################################################################################################
+''' 
+	Purpose:
+	---
+	This function will load  trained model and classify the event from an image which is 
+    sent as an input.
+	
+	Input Arguments:
+	---
+	`image`: Image path sent by input file 	
+	
+	Returns:
+	---
+	`event` : [ String ]
+						  Detected event is returned in the form of a string
+
+	Example call:
+	---
+	event = classify_event(image_path)
+	'''
+def classify_event(image):
+
+    event = "variable to return the detected function"
+    return event
+
+
+
+def classification(img_name_list):
+    for img_index in range(len(img_name_list)):
+        img = "events/" + str(img_name_list[img_index]) + ".jpeg"
+        detected_event = classify_event(img)
+        print((img_index + 1), detected_event)
+        if detected_event == combat:
+            detected_list.append("combat")
+        if detected_event == rehab:
+            detected_list.append("rehab")
+        if detected_event == military_vehicles:
+            detected_list.append("militaryvehicles")
+        if detected_event == fire:
+            detected_list.append("fire")
+        if detected_event == destroyed_building:
+            detected_list.append("destroyedbuilding")
+    shutil.rmtree('events')
+    return detected_list
+
+def detected_list_processing(detected_list):
+    try:
+        detected_events = open("detected_events.txt", "w")
+        detected_events.writelines(str(detected_list))
+    except Exception as e:
+        print("Error: ", e)
+
+def input_function():
+    if platform == "win32":
+        try:
+            subprocess.run("input.exe")
+        except Exception as e:
+            print("Error: ", e)
+    if platform == "linux":
+        try:
+            subprocess.run("./input")
+        except Exception as e:
+            print("Error: ", e)
+    img_names = open("image_names.txt", "r")
+    img_name_str = img_names.read()
+
+    img_name_list = ast.literal_eval(img_name_str)
+    return img_name_list
+    
+def output_function():
+    if platform == "win32":
+        try:
+            subprocess.run("output.exe")
+        except Exception as e:
+            print("Error: ", e)
+    if platform == "linux":
+        try:
+            subprocess.run("./output")
+        except Exception as e:
+            print("Error: ", e)
+
+
+def main():
+    ##### Input #####
+    img_name_list = input_function()
+    #################
+
+    ##### Process #####
+    detected_list = classification(img_name_list)
+    detected_list_processing(detected_list)
+    ###################
+
+    ##### Output #####
+    output_function()
+    ##################
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        if os.path.exists('events'):
+            shutil.rmtree('events')
+        sys.exit()
+###################################################################################################
 
